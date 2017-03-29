62507c59cc4e9420ec01f006fcf94a9af0b0f852
Matthew McGuff <matthewmcguff85@gmail.com>
2017-03-29 14:01:43 -0600

    mvp created.

--- FittnessFrog/Program.cs
+++ FittnessFrog/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace FittnessFrog
 {
@@ -10,9 +11,35 @@ namespace FittnessFrog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Frog");
-            Console.WriteLine("Hello Dogs");
+            var start = new Program();
+            start.RunProgram();
+
+        }
+
+        public void RunProgram()
+        {
+            string smin = "0";
+            int total = 0;
+           
+            //Repeat until User quits
+            while (smin != "x")
+            {
+                total = total + Convert.ToInt32(smin);
+                //Prompt user for minutes exercised
+                Console.WriteLine("How many mintues did you exercise?  Enter 'x' to Stop.");
+
+                //Add minutes exercised to total
+                smin = Console.ReadLine();
+                
+            }
+
+            //Display total minutes exercised to the screen
+            Console.WriteLine("You exercised " + total + " minutes.");
+
             Console.ReadKey();
         }
+
+        
+
     }
 }
